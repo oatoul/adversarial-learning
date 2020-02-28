c6892933f43c05d0e97309a6a4fa95a7a6406455
Luo Tao <tao.luo@sc.com>
2020-02-28 16:20:18 +0800

    done!

--- Assignment_1/mnist_code.py
+++ Assignment_1/mnist_code.py
@@ -13,8 +13,9 @@ from PIL import Image
 import sys
 import copy
 
-from torch.autograd import Variable
-from torch.autograd.gradcheck import zero_gradients
+from Assignment_1.deepfool import deepfool
+import matplotlib.pyplot as plt
+
 
 class Net(nn.Module):
     def __init__(self):
@@ -39,9 +40,17 @@ class Net(nn.Module):
         x = self.fc2(x)
         return x
 
+
 def numpy_loader(input):
-    item = np.load(input)/255.0
+    item = np.load(input) / 255.0
     return Image.fromarray(item)
+
+
+def clip_tensor(A, minv, maxv):
+    A = torch.max(A, minv * torch.ones(A.shape))
+    A = torch.min(A, maxv * torch.ones(A.shape))
+    return A
+
 
 def evaluate_model_for_accuracy(model, device, data_loader):
     model.eval()
@@ -64,12 +73,14 @@ def evaluate_adv_images(model, device, kwargs, mean, std, data_loader):
     model.eval()
 
     adv_data_loader = torch.utils.data.DataLoader(
-        torchvision.datasets.DatasetFolder('adv_images', #Change this to your adv_images folder
+        torchvision.datasets.DatasetFolder('adv_images',  # Change this to your adv_images folder
                                            loader=numpy_loader,
                                            extensions='.npy',
                                            transform=transforms.Compose([transforms.ToTensor(),
                                                                          transforms.Normalize(mean, std)])),
-                                                                         batch_size=batch_size, **kwargs)
+        batch_size=batch_size, **kwargs)
+
+    evaluate_model_for_accuracy(model, device, adv_data_loader)
 
     given_dataset = []
     adv_images = []
@@ -77,11 +88,11 @@ def evaluate_adv_images(model, device, kwargs, mean, std, data_loader):
     with torch.no_grad():
         for data, target in data_loader:
             data, target = data.to(device), target.to(device)
-            if len(given_dataset) ==0:
+            if len(given_dataset) == 0:
                 given_dataset = data.squeeze().detach().cpu().numpy()
             else:
                 given_dataset = np.concatenate([given_dataset, data.squeeze().detach().cpu().numpy()],
-                                           axis=0)
+                                               axis=0)
 
         for data, target in adv_data_loader:
             data, target = data.to(device), target.to(device)
@@ -91,7 +102,7 @@ def evaluate_adv_images(model, device, kwargs, mean, std, data_loader):
             adv_images = data
             labels = target
 
-    #Checking the range of generated images
+    # Checking the range of generated images
     adv_images_copy = copy.deepcopy(adv_images)
     for k in range(adv_images_copy.shape[0]):
         image_ = adv_images_copy[k, :, :]
@@ -109,128 +120,182 @@ def evaluate_adv_images(model, device, kwargs, mean, std, data_loader):
     adv_images = adv_images.squeeze().detach().cpu().numpy()
     labels = labels.squeeze().detach().cpu().numpy()
 
-
-    #Checking for equation 2 and equation 3
+    # Checking for equation 2 and equation 3
     if all([x > 0.8 for x in softmax_values.tolist()]):
         print('Softmax values for all of your adv images are greater than 0.8')
         S = 0
         for i in range(10):
-            label_indices = np.where(labels==i)[0]
+            label_indices = np.where(labels == i)[0]
             a_i = adv_images[label_indices, :, :]
             for k in range(10):
                 image = a_i[k, :, :]
                 S = S + np.min(
-                            np.sqrt(
-                                np.sum(
-                                    np.square(
-                                        np.subtract(given_dataset, np.tile(np.expand_dims(image, axis=0), [1000,1,1]))
-                                    ),axis=(1,2))))
+                    np.sqrt(
+                        np.sum(
+                            np.square(
+                                np.subtract(given_dataset, np.tile(np.expand_dims(image, axis=0), [1000, 1, 1]))
+                            ), axis=(1, 2))))
 
         print('Value of S : {:.4f}'.format(S / 100))
 
     else:
         print('Softmax values for some of your adv images are less than 0.8')
 
-
-
-def generate_adv_images():
+def generate_adv_images(mean, std, model, device, data_loader, kwargs):
     adv_images = []
     targeted_class_labels = []
     image_names = []
-    #your code to generate adv_images goes here
-    return adv_images,image_names,targeted_class_labels
+    # your code to generate adv_images goes here
+    #
+    # adv_images_all = []
+    # targeted_class_labels_all = []
+    # image_names_all = []
+    #
+    # cost = []
+    # given_dataset = []
+    # with torch.no_grad():
+    #     for data, target in data_loader:
+    #         data, target = data.to(device), target.to(device)
+    #         if len(given_dataset) == 0:
+    #             given_dataset = data.squeeze().detach().cpu().numpy()
+    #         else:
+    #             given_dataset = np.concatenate([given_dataset, data.squeeze().detach().cpu().numpy()],
+    #                                            axis=0)
+    #
+    # for data, target in data_loader:
+    #     data, target = data.to(device), target.to(device)
+    #     max_it = 50
+    #     for k in range(data.shape[0]):
+    #         image_ = data[k, :, :]
+    #
+    #         softmax_t, r, loop_i, label_orig, label_pert, pert_image = deepfool(image=image_, net=model.eval(),
+    #                                                                             max_iter=max_it)
+    #
+    #         if softmax_t > 0.8:
+    #             adv_images_all.append(pert_image)
+    #             targeted_class_labels_all.append(label_pert)
+    #             image_names_all.append(str(k) + "_" + str(label_orig))
+    #             cost.append(calculate_cost(pert_image, given_dataset))
+    #             # for t, m, s in zip(pert_image, mean, std):
+    #             #     t.mul_(s).add_(m)
+    #             # pert_image = clip_tensor(pert_image, 0, 1).squeeze().detach().cpu().numpy()
+    #             # pert_image = pert_image.squeeze().detach().cpu().numpy()
+    #             # pert_image = 255.0 * pert_image
+    #             #
+    #             # fpath = "adv_images/" + str(label_pert) + "/" + str(k)
+    #             # np.save(fpath, pert_image)
+    #         # if adv_images.__len__() >= 10:
+    #         #     break
+    #         print(str(k))
+    #
+    # save_data("adv_images_all", adv_images_all, image_names_all, targeted_class_labels_all, std, mean)
 
-def deepfool(image, net, num_classes=10, overshoot=0.02, max_iter=50):
-
-    """
-    https://github.com/LTS4/DeepFool/blob/master/Python/test_deepfool.py
-       :param image: Image of size HxWx3
-       :param net: network (input: images, output: values of activation **BEFORE** softmax).
-       :param num_classes: num_classes (limits the number of classes to test against, by default = 10)
-       :param overshoot: used as a termination criterion to prevent vanishing updates (default = 0.02).
-       :param max_iter: maximum number of iterations for deepfool (default = 50)
-       :return: minimal perturbation that fools the classifier, number of iterations that it required, new estimated_label and perturbed image
-    """
-    is_cuda = torch.cuda.is_available()
-
-    if is_cuda:
-        print("Using GPU")
-        image = image.cuda()
-        net = net.cuda()
-    else:
-        print("Using CPU")
 
 
-    f_image = net.forward(Variable(image[None, :, :, :], requires_grad=True)).data.cpu().numpy().flatten()
-    I = (np.array(f_image)).flatten().argsort()[::-1]
+    # batch_size = adv_images_all.__len__()
+    batch_size = 857
 
-    I = I[0:num_classes]
-    label = I[0]
+    adv_images, image_names, targeted_class_labels = select_adv_images(mean,std,device,data_loader,batch_size,kwargs)
 
-    input_shape = image.cpu().numpy().shape
-    pert_image = copy.deepcopy(image)
-    w = np.zeros(input_shape)
-    r_tot = np.zeros(input_shape)
+    # for i in range(10):
+    #     ind_i = np.where(targeted_class_labels == i).ravel()
+    #     cost_i = cost[ind_i]
+    #     ind_max = np.argpartition(cost_i, -10)[-10:]
+    #     ind_i_max = ind_i[ind_max]
+    #     adv_images.append(adv_images_all[ind_i_max])
+    #     image_names.append(image_names_all[ind_i_max])
+    #     targeted_class_labels.append(targeted_class_labels_all[ind_i_max])
 
-    loop_i = 0
+    return adv_images, image_names, targeted_class_labels
 
-    x = Variable(pert_image[None, :], requires_grad=True)
-    fs = net.forward(x)
-    fs_list = [fs[0,I[k]] for k in range(num_classes)]
-    k_i = label
 
-    while k_i == label and loop_i < max_iter:
+def select_adv_images(mean, std, device, data_loader, batch_size, kwargs):
+    adv_images_sel = []
+    targeted_class_labels_sel = []
+    image_names_sel = []
 
-        pert = np.inf
-        fs[0, I[0]].backward(retain_graph=True)
-        grad_orig = x.grad.data.cpu().numpy().copy()
+    adv_data_loader_all = torch.utils.data.DataLoader(
+        torchvision.datasets.DatasetFolder('adv_images_all',  # Change this to your adv_images folder
+                                           loader=numpy_loader,
+                                           extensions='.npy',
+                                           transform=transforms.Compose([transforms.ToTensor(),
+                                                                         transforms.Normalize(mean, std)])),
+        batch_size=batch_size, **kwargs)
 
-        for k in range(1, num_classes):
-            zero_gradients(x)
 
-            fs[0, I[k]].backward(retain_graph=True)
-            cur_grad = x.grad.data.cpu().numpy().copy()
+    given_dataset = []
+    adv_images = []
+    labels = []
 
-            # set new w_k and new f_k
-            w_k = cur_grad - grad_orig
-            f_k = (fs[0, I[k]] - fs[0, I[0]]).data.cpu().numpy()
+    with torch.no_grad():
+        for data, target in data_loader:
+            data, target = data.to(device), target.to(device)
+            if len(given_dataset) == 0:
+                given_dataset = data.squeeze().detach().cpu().numpy()
+            else:
+                given_dataset = np.concatenate([given_dataset, data.squeeze().detach().cpu().numpy()],
+                                               axis=0)
 
-            pert_k = abs(f_k)/np.linalg.norm(w_k.flatten())
+        for data, target in adv_data_loader_all:
+            data, target = data.to(device), target.to(device)
+            adv_images = data
+            labels = target
 
-            # determine which w_k to use
-            if pert_k < pert:
-                pert = pert_k
-                w = w_k
+    for i in range(10):
+        label_indices = np.where(labels == i)[0]
+        a_i = adv_images[label_indices, :, :]
 
-        # compute r_i and r_tot
-        # Added 1e-4 for numerical stability
-        r_i =  (pert+1e-4) * w / np.linalg.norm(w)
-        r_tot = np.float32(r_tot + r_i)
+        cost_i = []
+        for k in range(a_i.__len__()):
+            image = a_i[k, :, :]
+            cost_k = calculate_cost(image, given_dataset)
+            cost_i.append(cost_k)
 
-        if is_cuda:
-            pert_image = image + (1+overshoot)*torch.from_numpy(r_tot).cuda()
-        else:
-            pert_image = image + (1+overshoot)*torch.from_numpy(r_tot)
+        ind = np.argpartition(cost_i, -10)[-10:]
+        # ind = np.argpartition(cost_i, 10)[:10]
+        selected_images_i = a_i[ind, :, :]
 
-        x = Variable(pert_image, requires_grad=True)
-        fs = net.forward(x)
-        k_i = np.argmax(fs.data.cpu().numpy().flatten())
+        adv_images_sel.extend(selected_images_i)
+        targeted_class_labels_sel.extend(np.array([i]*10))
+        image_names_sel.extend(np.array(['0','1','2','3','4','5','6','7','8','9']))
 
-        loop_i += 1
+    return adv_images_sel, image_names_sel, targeted_class_labels_sel
 
-    r_tot = (1+overshoot)*r_tot
 
-    return r_tot, loop_i, label, k_i, pert_image
+
+
+def calculate_cost(image, given_dataset):
+    image_ = image.squeeze().detach().cpu().numpy()
+    return np.min(np.sqrt(np.sum(np.square(
+        np.subtract(given_dataset, np.tile(np.expand_dims(image_, axis=0), [1000, 1, 1]))), axis=(1, 2))))
+
+
+def save_data(save_folder, images, image_names, class_labels, std, mean):
+    for image, image_name, class_label in zip(images, image_names, class_labels):
+        for t, m, s in zip(image, mean, std):
+            t.mul_(s).add_(m)
+
+        image_to_save = image.squeeze().detach().cpu().numpy()
+        image_to_save = 255.0 * image_to_save
+
+        if np.min(image_to_save) < 0 or np.max(image_to_save) > 255:
+            print('Generated adversarial image is out of range.')
+            sys.exit()
+
+        if not os.path.exists(os.path.join(save_folder, str(class_label))):
+            makedirs(os.path.join(save_folder, str(class_label)))
+
+        np.save(os.path.join(save_folder, str(class_label), image_name), image_to_save)
+
 
 def main():
     # Settings
     parser = argparse.ArgumentParser()
-    parser.add_argument('--batch-size', type=int, default=512)
-    parser.add_argument('--no-cuda', action='store_true', default=False,help='disables CUDA training')
+    parser.add_argument('--batch-size', type=int, default=1000)
+    parser.add_argument('--no-cuda', action='store_true', default=False, help='disables CUDA training')
     parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
     parser.add_argument('--model_path', type=str, default='model/mnist_cnn.pt')
     parser.add_argument('--data_folder', type=str, default='data')
-
 
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
@@ -242,27 +307,27 @@ def main():
     device = torch.device("cuda" if use_cuda else "cpu")
 
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-    data_loader =  torch.utils.data.DataLoader(
+    data_loader = torch.utils.data.DataLoader(
         torchvision.datasets.DatasetFolder('data',
-                                           loader= numpy_loader,
-                                           extensions= '.npy',
+                                           loader=numpy_loader,
+                                           extensions='.npy',
                                            transform=transforms.Compose([transforms.ToTensor(),
-                                           transforms.Normalize(mean, std)])),
-                                           batch_size=args.batch_size, **kwargs)
+                                                                         transforms.Normalize(mean, std)])),
+        batch_size=args.batch_size, **kwargs)
 
     model = Net().to(device)
 
-    model.load_state_dict(torch.load(args.model_path, map_location="cpu"))
+    model.load_state_dict(torch.load(args.model_path, map_location=torch.device('cpu')))
 
     evaluate_model_for_accuracy(model, device, data_loader)
 
-    adv_images,image_names,class_labels = generate_adv_images()
-    #Implement this method to generate adv images
-    #statisfying constraints mentioned in the assignment discription
+    adv_images, image_names, class_labels = generate_adv_images(mean, std, model, device, data_loader, kwargs)
+    # Implement this method to generate adv images
+    # statisfying constraints mentioned in the assignment discription
 
     save_folder = 'adv_images'
 
-    for image,image_name,class_label in zip(adv_images,image_names,class_labels):
+    for image, image_name, class_label in zip(adv_images, image_names, class_labels):
         for t, m, s in zip(image, mean, std):
             t.mul_(s).add_(m)
 
@@ -273,12 +338,12 @@ def main():
             print('Generated adversarial image is out of range.')
             sys.exit()
 
-        if not os.path.exists(os.path.join(save_folder,str(class_label))):
-            makedirs(os.path.join(save_folder,str(class_label)))
+        if not os.path.exists(os.path.join(save_folder, str(class_label))):
+            makedirs(os.path.join(save_folder, str(class_label)))
 
-        np.save(os.path.join(save_folder,str(class_label),image_name), image_to_save)
+        np.save(os.path.join(save_folder, str(class_label), image_name), image_to_save)
 
-    evaluate_adv_images(model,device,kwargs,mean,std,data_loader)
+    evaluate_adv_images(model, device, kwargs, mean, std, data_loader)
 
 
 if __name__ == '__main__':
